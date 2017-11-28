893769631efe00d5b873a4b68bd966a706fdb973
Skyler Knecht <skylerknecht@outlook.com>
2017-11-27 20:00:00 -0500

    Update CompilerListener.java

--- CompilerListener.java
+++ CompilerListener.java
@@ -14,6 +14,8 @@ public class CompilerListener extends MountCBaseListener {
   //  It stores the number of arguments in the formal argument list
   //  for a function, using the function's name as the key.
   private HashMap<String, Integer> symtab =  new HashMap<>();
+  private int ifIDGenerator = 1;
+  public int ifID = 1;
 
   @Override
   public void enterProgram(MountCParser.ProgramContext ctx) {
@@ -71,33 +73,24 @@ public class CompilerListener extends MountCBaseListener {
   public void exitFunCall(MountCParser.FunCallContext ctx) {
       String id = ctx.getParent().getChild(0).toString();
       System.out.println("\tCALL\t" + id);
-      int numBytesToPop = 2*(symtab.get(id) + 1);  //  Won't get here if id not in symtab.
+      int numBytesToPop = 2*(symtab.get(id) + 1);
       System.out.println("\tADDSP\t" + numBytesToPop + ",i");
       System.out.println("\tLDWA\t-2,s");
   }
 
-  @Override public void enterExprList(MountCParser.ExprListContext ctx) {
-      //System.out.println("Entering..");
-      //if(ctx.getParent().getChild(0).getChild(0).toString().equals("-")){
-      //  System.out.println("\tNEGA");
-      //}
-      //System.out.println("\tADDA\t0,s");
-      //System.out.println("\tSTWA\t0,s");
-  }
   @Override public void exitExprList(MountCParser.ExprListContext ctx) {
-    //System.out.println("\tExiting Expr List");
-    if(ctx.getParent().getParent().getChild(0).getChild(0).toString().equals("-")){
+    if(ctx.getParent().getChild(0).getChild(0).toString().equals("-")){
       System.out.println("\tNEGA");
     }
-    System.out.println("\tADDA\t0,s");
-    System.out.println("\tADDSP\t2,i");
-  //  putNumParams = 0;
- }
+    if(ctx.getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class) || ctx.getParent().getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class)){
+      System.out.println("\tADDA\t0,s");
+      System.out.println("\tADDSP\t2,i");
+    }
+  }
 
 
   @Override
   public void enterExpr_tail(MountCParser.Expr_tailContext ctx) {
-      //System.out.println("Info: " + ctx.getParent().getParent().getClass().getName());
       if(!ctx.getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class)){
           System.out.println("\tSUBSP\t2,i");
           System.out.println("\tSTWA\t0,s");
@@ -113,7 +106,6 @@ public class CompilerListener extends MountCBaseListener {
 
   @Override
   public void exitExpr_tail(MountCParser.Expr_tailContext ctx) {
-      //System.out.println("Info: " + ctx.getParent().getParent().getClass().getName());
       if(ctx.getChild(1).getChildCount() == 1){
         if(ctx.getChild(0).getChild(0).toString().equals("-")){
           System.out.println("\tNEGA\t");
@@ -134,6 +126,36 @@ public class CompilerListener extends MountCBaseListener {
   public void enterEmptyArglist(MountCParser.EmptyArglistContext ctx) {
     System.out.println("\tSUBSP\t2,i");
     System.out.println("\tSTWA\t0,s");
+  }
+
+
+
+  //  If Statments ------------
+  @Override
+  public void enterIfExpr(MountCParser.IfExprContext ctx) {
+  	ifIDGenerator=((ifIDGenerator/ifID)+1)*ifID;
+  }
+
+  @Override
+  public void exitIfExpr(MountCParser.IfExprContext ctx) {
+  	System.out.println("__end"+ifIDGenerator+":\tNOP0");
+  	ifIDGenerator= ((ifIDGenerator/ifID)-1)*ifID;
+  	if (ifIDGenerator/ifID == 1){
+  		ifID = ifID*10;
+  	}
+  }
+
+  @Override
+  public void enterThenPart(MountCParser.ThenPartContext ctx) {
+      System.out.println("\tCPWA\t0,i");
+      System.out.println("\tBREQ\t__else"+ifIDGenerator);
+
+  }
+
+  @Override
+  public void enterElsePart(MountCParser.ElsePartContext ctx) {
+  	System.out.println("\tBR\t__end"+ifIDGenerator);
+  	System.out.println("__else"+ifIDGenerator+":\tNOP0");
   }
 
 
