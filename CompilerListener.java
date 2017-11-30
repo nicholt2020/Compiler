b4c61c0709ccce79103dbce83cc5a480977efe72
Skyler Knecht <skylerknecht@outlook.com>
2017-11-29 21:30:35 -0500

    Update CompilerListener.java

--- CompilerListener.java
+++ CompilerListener.java
@@ -14,23 +14,61 @@ public class CompilerListener extends MountCBaseListener {
   //  It stores the number of arguments in the formal argument list
   //  for a function, using the function's name as the key.
   private HashMap<String, Integer> symtab =  new HashMap<>();
+  private HashMap<String, Integer> params =  new HashMap<>();
+
   private int ifIDGenerator = 1;
-  public int ifID = 1;
+  private int ifID = 1;
+  private String functionName = null;
+  private int offset = 0;
+
+  private void push(Boolean dontSTWA){
+    System.out.println("\tSUBSP\t2,i");
+    if(!dontSTWA){
+      System.out.println("\tSTWA\t0,s");
+    }
+    offset+=2;
+  }
+
+  private void pop(int numBytesToPop){
+    System.out.println("\tADDSP\t" + numBytesToPop + ",i");
+    offset-=numBytesToPop;
+  }
 
   @Override
   public void enterProgram(MountCParser.ProgramContext ctx) {
+    System.out.print("\n");
+    System.out.println(";Start Of Main Method");
     System.out.println("\tSUBSP\t2,i");
     System.out.println("\tCALL\tmain");
     System.out.println("\tADDSP\t2,i");
     System.out.println("\tSTOP");
-    System.out.println("putint:\tDECO\t2,s");
+    System.out.print("\n");
+    System.out.println(";putint will print out an integer value.");
+    System.out.println("putint:\tDECO\t2,s"); // putInt()
     System.out.println("\tLDWA\t2,s");
-    System.out.println("\tSTWA\t4,s");  //  return the same value we printed out.
+    System.out.println("\tSTWA\t4,s");
     System.out.println("\tRET");
     symtab.put("putint", 1);
-    System.out.println("getint:\tDECI\t2,s");
+    System.out.print("\n");
+    System.out.println(";getint will retrieve input.");
+    System.out.println("getint:\tDECI\t2,s"); // getint()
     System.out.println("\tRET");
     symtab.put("getint", 0);
+    System.out.print("\n");
+    System.out.println(";getchar will retrieve input and store the ASCII equivelant.");
+    System.out.println("getchar:\tLDWA\t0,i"); // getChar()
+    System.out.println("\tLDBA\tcharIn,d");
+    System.out.println("\tSTWA\t2,s");
+    System.out.println("\tRET");
+    symtab.put("getchar", 0);
+    System.out.print("\n");
+    System.out.println(";putchar will print out an ASCII equivelant.");
+    System.out.println("putchar:\tLDWA\t2,s"); // putChar()
+    System.out.println("\tSTBA\tcharOut,d");
+    System.out.println("\tSTWA\t4,s");
+    System.out.println("\tRET");
+    symtab.put("putchar", 1);
+
   }
   @Override
   public void exitProgram(MountCParser.ProgramContext ctx) {
@@ -39,12 +77,43 @@ public class CompilerListener extends MountCBaseListener {
 
   @Override
   public void enterFun_def(MountCParser.Fun_defContext ctx) {
-    String id = ctx.ID().toString();
-    System.out.println(id + ":\tNOP0");
-    if(!symtab.containsKey(id)){
-      symtab.put(id, 0);
+    String id = ctx.ID().toString(); // Get the ID We're working with ex: MAIN
+
+    int numParams;
+    if(ctx.getChild(2).getChildCount() != 0){ // If the Num Params is not 0 then calculate the Num Paramas
+      numParams = (ctx.getChild(2).getChildCount() / 2) + 1;
+    } else {
+      numParams = 0;
     }
+
+    if(!symtab.containsKey(id)){ // If function not already inside symtab add it in.
+      symtab.put(id, numParams);
+    }
+
+    int numBytes = 2*(symtab.get(id) + 1) - 2;
+
+    for(int i = 0; i < ctx.getChild(2).getChildCount(); i += 2){ // Loop through the number of params and Declare them all in PEP 9
+      System.out.println(";"+id + "_" + ctx.getChild(2).getChild(i).toString() +" "+numBytes);
+      params.put(id + "_" + ctx.getChild(2).getChild(i).toString(), numBytes);
+      numBytes -= 2;
+      //System.out.println(id + "_" + ctx.getChild(2).getChild(i).toString() + ", " + i);
+    }
+
+    System.out.print("\n");
+    System.out.println(id + ":\tNOP0");
+
+    functionName = id;
   }
+
+  @Override
+  public void exitEquExp(MountCParser.EquExpContext ctx) {
+    //System.out.println(ctx.getParent().getChild(0).toString());
+      if(params.containsKey(functionName+"_"+ctx.getParent().getChild(0).toString())){
+          int location = params.get(functionName+"_"+ctx.getParent().getChild(0).toString()) + offset;
+          System.out.println("\tSTWA\t" + location + ",s");
+      }
+  }
+
 
   @Override
   public void exitFun_def(MountCParser.Fun_defContext ctx) {
@@ -58,6 +127,17 @@ public class CompilerListener extends MountCBaseListener {
   }
 
   @Override
+  public void enterIdTerm(MountCParser.IdTermContext ctx) {
+    if(params.containsKey(functionName + "_" + ctx.getChild(0).toString())){
+    // System.out.println(functionName + "_" + ctx.getChild(0).toString());
+        int location = params.get(functionName+ "_" +ctx.getChild(0).toString())+ offset;
+        System.out.println("\tLDWA\t" + location + ",s");
+        System.out.println("\tSTWA\t" + location + ",s");
+    }
+  }
+
+
+  @Override
   public void enterFunCall(MountCParser.FunCallContext ctx) {
     String id = ctx.getParent().getChild(0).toString();
     Integer numParams = symtab.get(id);
@@ -69,7 +149,7 @@ public class CompilerListener extends MountCBaseListener {
     //   System.err.println("function " + id + " called with wrong number of arguments");
     //   System.exit(2);
     //}
-    System.out.println("\tSUBSP\t2,i");
+    push(true);
   }
 
   @Override
@@ -77,7 +157,7 @@ public class CompilerListener extends MountCBaseListener {
       String id = ctx.getParent().getChild(0).toString();
       System.out.println("\tCALL\t" + id);
       int numBytesToPop = 2*(symtab.get(id) + 1);
-      System.out.println("\tADDSP\t" + numBytesToPop + ",i");
+      pop(numBytesToPop);
       System.out.println("\tLDWA\t-2,s");
   }
 
@@ -85,9 +165,11 @@ public class CompilerListener extends MountCBaseListener {
     if(ctx.getParent().getChild(0).getChild(0).toString().equals("-")){
       System.out.println("\tNEGA");
     }
+
+    // When you exit an exprList add the current subtotal to the Accumulator and cleanup the subtotal with ADDSP.
     if(ctx.getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class) || ctx.getParent().getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class)){
       System.out.println("\tADDA\t0,s");
-      System.out.println("\tADDSP\t2,i");
+      pop(2);
     }
   }
 
@@ -95,8 +177,7 @@ public class CompilerListener extends MountCBaseListener {
   @Override
   public void enterExpr_tail(MountCParser.Expr_tailContext ctx) {
       if(!ctx.getParent().getParent().getClass().equals(MountCParser.Expr_tailContext.class)){
-          System.out.println("\tSUBSP\t2,i");
-          System.out.println("\tSTWA\t0,s");
+          push(false);
       } else {
           if(ctx.getParent().getParent().getChild(0).getChild(0).toString().equals("-")){
              System.out.println("\tNEGA");
@@ -115,20 +196,18 @@ public class CompilerListener extends MountCBaseListener {
         }
           System.out.println("\tADDA\t0,s");
           System.out.println("\tSTWA\t0,s");
-          System.out.println("\tADDSP\t2,i");
+          pop(2);
       }
   }
 
   @Override
-  public void enterActualArg(MountCParser.ActualArgContext ctx) {
-    System.out.println("\tSUBSP\t2,i");
-    System.out.println("\tSTWA\t0,s");
+  public void enterActualArg(MountCParser.ActualArgContext ctx){
+    push(false);
   }
 
   @Override
   public void enterEmptyArglist(MountCParser.EmptyArglistContext ctx) {
-    System.out.println("\tSUBSP\t2,i");
-    System.out.println("\tSTWA\t0,s");
+    push(false);
   }
 
 
@@ -144,7 +223,7 @@ public class CompilerListener extends MountCBaseListener {
   	System.out.println("__end"+ifIDGenerator+":\tNOP0");
   	ifIDGenerator= ((ifIDGenerator/ifID)-1)*ifID;
   	if (ifIDGenerator/ifID == 1){
-  		ifID = ifID*100;
+  		ifID = ifID*1000;
   	}
   }
 
