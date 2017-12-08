d468d32b59160e95329386eae4f47e999e84cd22
Skyler Knecht <skylerknecht@outlook.com>
2017-12-08 09:16:36 -0500

    Update CompilerListener.java

--- MountCCompiler/CompilerListener.java
+++ MountCCompiler/CompilerListener.java
@@ -1,136 +1,113 @@
--/*
--   This listener generates Pep/9 assembly for a (mini) MountC program.
--   Of the four i/o functions of MountC, it only predefines putint.
--   K. Weber
--   weberk@mountunion.edu
--   10-nov-2017
--*/
--
--import java.util.HashMap;
--
--public class CompilerListener extends MountCBaseListener {
--
--  /*  Here is a primitive symbol table for function definitions.
--   *  It stores the number of arguments in the formal argument list
--      for a function, using the function's name as the key.
--  */
--  private HashMap<String, Integer> symtab =  new HashMap<>();
--   
--  /* Here is a primitive symbol table for parameters.
--   * It stores the name of var EX: x for skyFun would be stored as "skyFun_x".
--   * It also stores the position of the parameter on the stack frame from the stack pointer.
--  */
--  private HashMap<String, Integer> params =  new HashMap<>();
--
--   
--  // 
--  private int ifIDGenerator = 1;
--   
--  //
--  private int ifID = 1;
--   
--  /* functionName is used to record the name of the current function whenever you enter a
--   * function call so you can acturately retrieve variables from the params HashMap.
--  */ 
--  private String functionName = null;
--   
--  /* Offset is used to calculate the distance to parameters when other changes are made to the 
--   * stack pointer after variable declaration.
--  */
--  private int offset = 0;
--
--  /* Push is used whenever you need to "make room" on the stack for some operation.
--   * Push accepts a Boolean parameter as you may not always want to store the accumulator when 
--     you "make room".
--   * Push also adds to the offset to keep track of variable location.
--  */ 
--  private void push(Boolean dontSTWA){
--    System.out.println("\tSUBSP\t2,i");
--    if(!dontSTWA){
--      System.out.println("\tSTWA\t0,s");
--    }
--    offset+=2;
--  }
--
--  /* Pop is used whenever you need to "clean-up" the stack for some operation.
--   * Pop accepts an integer parameter that is used to pass through how many bytes to add to the 
--     stack pointer.
--   * Pop also subtracts from the offset to keep track of variable location.
--  */
--  private void pop(int numBytesToPop){
--    System.out.println("\tADDSP\t" + numBytesToPop + ",i");
--    offset-=numBytesToPop;
--  }
--
--  /* enterProgram is used to setup the initials methods of any legal MountC Program.
--   * Methods defined here are: putint(), getint(), getchar() and putchar().
--  */  -/*
--   This listener generates Pep/9 assembly for a (mini) MountC program.
--   Of the four i/o functions of MountC, it only predefines putint.
--   K. Weber
--   weberk@mountunion.edu
--   10-nov-2017
--*/
--
--import java.util.HashMap;
--
--public class CompilerListener extends MountCBaseListener {
--
--  /*  Here is a primitive symbol table for function definitions.
--   *  It stores the number of arguments in the formal argument list
--      for a function, using the function's name as the key.
--  */
--  private HashMap<String, Integer> symtab =  new HashMap<>();
--   
--  /* Here is a primitive symbol table for parameters.
--   * It stores the name of var EX: x for skyFun would be stored as "skyFun_x".
--   * It also stores the position of the parameter on the stack frame from the stack pointer.
--  */
--  private HashMap<String, Integer> params =  new HashMap<>();
--
--   
--  // 
--  private int ifIDGenerator = 1;
--   
--  //
--  private int ifID = 1;
--   
--  /* functionName is used to record the name of the current function whenever you enter a
--   * function call so you can acturately retrieve variables from the params HashMap.
--  */ 
--  private String functionName = null;
--   
--  /* Offset is used to calculate the distance to parameters when other changes are made to the 
--   * stack pointer after variable declaration.
--  */
--  private int offset = 0;
--
--  /* Push is used whenever you need to "make room" on the stack for some operation.
--   * Push accepts a Boolean parameter as you may not always want to store the accumulator when 
--     you "make room".
--   * Push also adds to the offset to keep track of variable location.
--  */ 
--  private void push(Boolean dontSTWA){
--    System.out.println("\tSUBSP\t2,i");
--    if(!dontSTWA){
--      System.out.println("\tSTWA\t0,s");
--    }
--    offset+=2;
--  }
--
--  /* Pop is used whenever you need to "clean-up" the stack for some operation.
--   * Pop accepts an integer parameter that is used to pass through how many bytes to add to the 
--     stack pointer.
--   * Pop also subtracts from the offset to keep track of variable location.
--  */
--  private void pop(int numBytesToPop){
--    System.out.println("\tADDSP\t" + numBytesToPop + ",i");
--    offset-=numBytesToPop;
--  }
--
--  /* enterProgram is used to setup the initials methods of any legal MountC Program.
--   * Methods defined here are: putint(), getint(), getchar() and putchar().
--  */
+/*
+   This listener generates Pep/9 assembly for a (mini) MountC program.
+   Of the four i/o functions of MountC, it only predefines putint.
+   K. Weber
+   weberk@mountunion.edu
+   10-nov-2017
+*/
+
+import java.util.HashMap;
+
+public class CompilerListener extends MountCBaseListener {
+
+  //  Here is a primitive symbol table for function definitions.
+  //  It stores the number of arguments in the formal argument list
+  //  for a function, using the function's name as the key.
+  private HashMap<String, Integer> symtab =  new HashMap<>();
+  private HashMap<String, Integer> params =  new HashMap<>();
+
+  private int ifIDGenerator = 1;
+  private int ifID = 1;
+  private String functionName = null;
+  private int offset = 0;
+
+  private void push(Boolean dontSTWA){
+    System.out.println("\tSUBSP\t2,i");
+    if(!dontSTWA){
+      System.out.println("\tSTWA\t0,s");
+    }
+    offset+=2;
+  }
+
+  private void pop(int numBytesToPop){
+    System.out.println("\tADDSP\t" + numBytesToPop + ",i");
+    offset-=numBytesToPop;
+  }
+
+  @Override
+  public void enterProgram(MountCParser.ProgramContext ctx) {
+    System.out.print("\n");
+    System.out.println(";Start Of Main Method");
+    System.out.println("\tSUBSP\t2,i");
+    System.out.println("\tCALL\tmain");
+    System.out.println("\tADDSP\t2,i");
+    System.out.println("\tSTOP");
+    System.out.print("\n");
+    System.out.println(";putint will print out an integer value.");
+    System.out.println("putint:\tDECO\t2,s"); // putInt()
+    System.out.println("\tLDWA\t2,s");
+    System.out.println("\tSTWA\t4,s");
+    System.out.println("\tRET");
+    symtab.put("putint", 1);
+    System.out.print("\n");
+    System.out.println(";getint will retrieve input.");
+    System.out.println("getint:\tDECI\t2,s"); // getint()
+    System.out.println("\tRET");
+    symtab.put("getint", 0);
+    System.out.print("\n");
+    System.out.println(";getchar will retrieve input and store the ASCII equivelant.");
+    System.out.println("getchar:\tLDWA\t0,i"); // getChar()
+    System.out.println("\tLDBA\tcharIn,d");
+    System.out.println("\tSTWA\t2,s");
+    System.out.println("\tRET");
+    symtab.put("getchar", 0);
+    System.out.print("\n");
+    System.out.println(";putchar will print out an ASCII equivelant.");
+    System.out.println("putchar:\tLDWA\t2,s"); // putChar()
+    System.out.println("\tSTBA\tcharOut,d");
+    System.out.println("\tSTWA\t4,s");
+    System.out.println("\tRET");
+    symtab.put("putchar", 1);
+
+  }
+  @Override
+  public void exitProgram(MountCParser.ProgramContext ctx) {
+    System.out.println("\t.END");
+  }
+
+  @Override
+  public void enterFun_def(MountCParser.Fun_defContext ctx) {
+    offset = 0;
+    String id = ctx.ID().toString(); // Get the ID We're working with ex: MAIN
+
+    int numParams;
+    if(ctx.getChild(2).getChildCount() != 0){ // If the Num Params is not 0 then calculate the Num Paramas
+      numParams = (ctx.getChild(2).getChildCount() / 2) + 1;
+    } else {
+      numParams = 0;
+    }
+
+    if(!symtab.containsKey(id)){ // If function not already inside symtab add it in.
+      symtab.put(id, numParams);
+    } else {
+      System.err.println("function " + id + " already defined at this point");
+      System.exit(5);
+    }
+
+    int numBytes = 2*(symtab.get(id) + 1) - 2;
+
+    for(int i = 0; i < ctx.getChild(2).getChildCount(); i += 2){ // Loop through the number of params and Declare them all in PEP 9
+      //System.out.println(";"+id + "_" + ctx.getChild(2).getChild(i).toString() +" "+numBytes);
+      params.put(id + "_" + ctx.getChild(2).getChild(i).toString(), numBytes);
+      numBytes -= 2;
+      //System.out.println(id + "_" + ctx.getChild(2).getChild(i).toString() + ", " + i);
+    }
+
+    System.out.print("\n");
+    System.out.println(id + ":\tNOP0");
+
+    functionName = id;
+  }
 
   @Override
   public void exitEquExp(MountCParser.EquExpContext ctx) {
